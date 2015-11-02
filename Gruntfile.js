38bd4a6d7e2de4fe2b19ac57b2d2e71e5b764712
Martin Kuric <martin.chix.kuric@gmail.com>
2015-11-02 14:49:35 +0100

    frontend stack setup

---
+++ Gruntfile.js
@@ -0,0 +1,104 @@
+module.exports = function(grunt) {
+	'use strict';
+
+	require('load-grunt-tasks')(grunt);
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		bowercopy: {
+			options: {
+				runBower: true,
+				clean: false
+			},
+			js: {
+				options: {
+					destPrefix: 'static/js/lib'
+				},
+				files: {
+					'000_jquery.min.js': 'jquery/dist/jquery.min.js'
+				}
+			}
+		},
+
+		sass: {
+			dev: {
+				options:{
+					style:'expanded',
+					sourcemap: 'inline',
+					precision: 10
+				},
+				files: [{
+					expand: true,
+					cwd: 'static/scss',
+					src: ['styles.scss'],
+					dest: 'static/css',
+					ext: '.css'
+				}]
+			},
+			dist: {
+				options: {
+					style:'compressed',
+					sourcemap: 'none',
+					precision: 10
+				},
+				files: [{
+					expand: true,
+					cwd: 'static/scss',
+					src: ['styles.scss'],
+					dest: 'static/css',
+					ext: '.min.css'
+				}]
+			}
+		},
+
+		uglify: {
+			dev: {
+				options: {
+					sourceMap: true
+				},
+				files: {
+					'static/js/scripts.js': [
+						'static/js/lib/*js',
+						'static/js/partials/*.js'
+					]
+				}
+			},
+			dist: {
+				options: {
+					sourceMap: false
+				},
+				files: {
+					'static/js/scripts.min.js': [
+						'static/js/lib/*js',
+						'static/js/partials/*.js'
+					]
+				}
+			}
+		},
+
+		watch: {
+			options: {
+				livereload: true
+			},
+			html: {
+				files: ['static/html/*.html']
+			},
+			css: {
+				files: ['static/scss/partials/*.scss', 'static/scss/styles.scss'],
+				tasks: ['sass:dev']
+			},
+			js: {
+				files: ['static/js/partials/*.js'],
+				tasks: ['uglify:dev']
+			}
+		}
+	});
+
+	grunt.event.on('watch', function(action, filepath, target) {
+		grunt.log.writeln(target + ': ' + filepath + ' has ' + action);
+	});
+
+	grunt.registerTask('dev',['bowercopy', 'sass:dev', 'uglify:dev', 'watch']);
+	grunt.registerTask('prod',['bowercopy', 'sass:dev', 'uglify:dev', 'sass:dist', 'uglify:dist']);
+};
