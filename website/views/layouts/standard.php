4201db530f40a2b77d4b6929150e3f42834cfd57
Martin Kuric <martin.chix.kuric@gmail.com>
2015-11-03 16:57:27 +0100

    deployment

--- website/views/layouts/standard.php
+++ website/views/layouts/standard.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html lang="<?= $this->language; ?>">
 	<head>
-		<meta charset="utf-8">	
-		<link rel="stylesheet" type="text/css" href="/static/css/styles.min.css">
-		<script type="text/javascript" src="/static/js/scripts.min.js"></script>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="/static/css/styles<?if(!PIMCORE_DEBUG) echo '.min';?>.css">
+		<script type="text/javascript" src="/static/js/scripts<?if(!PIMCORE_DEBUG) echo '.min';?>.js"></script>
 	</head>
 	<body>
 		<?= $this->layout()->content; ?>
