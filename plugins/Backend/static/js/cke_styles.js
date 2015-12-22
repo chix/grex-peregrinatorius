38da8690e2ab07157fb0109b7872543fd3b9b2cd
Martin Kuric <martin.kuric@portadesign.cz>
2015-12-22 20:18:07 +0100

    img left and right positioning in wysiwyg

--- plugins/Backend/static/js/cke_styles.js
+++ plugins/Backend/static/js/cke_styles.js
@@ -1,2 +1,4 @@
 CKEDITOR.stylesSet.add( 'websiteEditorStyles', [
+	{ name: 'Obrázek - vlevo', element: 'img', attributes: { 'class': 'grex-img-left' } },
+	{ name: 'Obrázek - vpravo', element: 'img', attributes: { 'class': 'grex-img-right' } }
 ] );
