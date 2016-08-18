596ff9b56c4d69dad8a71865c1caff8010883697
Martin Kuric <martin.chix.kuric@gmail.com>
2016-08-18 15:41:06 +0200

    use pimcore headLink and headScript helpers with built-in cache buster

--- website/views/layouts/snippet.php
+++ website/views/layouts/snippet.php
@@ -4,13 +4,17 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 
-		<link rel="stylesheet" type="text/css" href="/static/css/styles<?if(!PIMCORE_DEBUG) echo '.min';?>.css">
+		<?= $this->headLink()
+			->appendStylesheet('/static/css/styles' . ((!PIMCORE_DEBUG) ? '.min.css' : '.css'), 'screen');
+		?>
 
 		<link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 		<link href='http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
 		<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
 
-		<script type="text/javascript" src="/static/js/scripts<?if(!PIMCORE_DEBUG) echo '.min';?>.js"></script>
+		<?= $this->headScript()
+			->appendFile('/static/js/scripts' . ((!PIMCORE_DEBUG) ? '.min.js' : '.js'));
+		?>
 
 		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
