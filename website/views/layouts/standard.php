6ebdb0b4e281c429805cc40f69a74355dbdc0e57
Martin Kuric <martin.chix.kuric@gmail.com>
2016-09-27 13:37:48 +0200

    load external resources over https

--- website/views/layouts/standard.php
+++ website/views/layouts/standard.php
@@ -24,20 +24,9 @@
 			->appendStylesheet('/static/css/styles' . ((!PIMCORE_DEBUG) ? '.min.css' : '.css'), 'screen');
 		?>
 
-		<link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-		<link href='http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-		<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
-
-		<?= $this->headScript()
-			->appendFile('/static/js/scripts' . ((!PIMCORE_DEBUG) ? '.min.js' : '.js'));
-		?>
-
-		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-		<!--[if lt IE 9]>
-			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-		<![endif]-->
+		<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+		<link href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css">
 	</head>
 	<body>
 		<?=$this->render('snippets/nav.php');?>
@@ -58,5 +47,16 @@
 				ga('send', 'pageview');
 			</script>
 		<?endif;?>
+
+		<?= $this->headScript()
+			->appendFile('/static/js/scripts' . ((!PIMCORE_DEBUG) ? '.min.js' : '.js'));
+		?>
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
 	</body>
 </html>
